b1ef8d80ebb672f76fb02fd73a42a43f4db72572
cr34t1ve <cr34t1ve@protonmail.com>
2021-06-22 02:17:10 +0000

    'profile' page constructed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:safo_profileui/screens/home/home.dart';
+import 'package:flutter/services.dart';
+import 'package:safo_profileui/screens/profile/profile.dart';
 
 void main() {
+   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
   runApp(MaterialApp(
-    home: Home(),
+    home: Profile(),
   ));
 }
 
---
+++ lib/screens/profile/profile.dart
@@ -0,0 +1,424 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                width: 375.0,
+                height: 283.0,
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'assets/images/keyboard_keys_backlight_141665_3840x2160@2x.png',
+                      height: 207,
+                      width: double.infinity,
+                    ),
+                    SizedBox(
+                      height: 35.0,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Kwabena Safo',
+                                style: TextStyle(
+                                    fontFamily: 'Poppins',
+                                    fontSize: 15.0,
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.black),
+                              ),
+                              Row(
+                                children: [
+                                  Container(
+                                    width: 74.0,
+                                    height: 26.0,
+                                    decoration: BoxDecoration(
+                                      gradient: LinearGradient(
+                                          begin: Alignment.topLeft,
+                                          end: Alignment.bottomRight,
+                                          colors: [
+                                            Color(0xFF2676D3),
+                                            Color(0xFFFEB9E5)
+                                          ]),
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(30.0)),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        'Follow',
+                                        style: TextStyle(
+                                            fontFamily: 'Poppins',
+                                            fontSize: 10.0,
+                                            fontWeight: FontWeight.w400,
+                                            color: Colors.white),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 12.0,
+                                  ),
+                                  Container(
+                                    width: 74.0,
+                                    height: 26.0,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(30.0)),
+                                        border: Border.all(
+                                            color: Color(0xFF5D5C5C),
+                                            width: 1.0)),
+                                    child: Center(
+                                      child: Text(
+                                        'Message',
+                                        style: TextStyle(
+                                            fontFamily: 'Poppins',
+                                            fontSize: 10.0,
+                                            fontWeight: FontWeight.w400,
+                                            color: Color(0xFF5D5C5C)),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                'Ui/Ux Designer',
+                                style: TextStyle(
+                                    fontFamily: 'Poppins',
+                                    fontSize: 10.0,
+                                    fontWeight: FontWeight.w400,
+                                    color: Color(0xFFC9C9C9)),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Positioned(
+                left: 20.5,
+                top: 35.5,
+                child: Icon(
+                  Icons.chevron_left_outlined,
+                  color: Colors.white,
+                  size: 35,
+                ),
+              ),
+              Positioned(
+                left: 20.0,
+                bottom: 40.0,
+                child: Image.asset('assets/images/2020-06-08 14.58.47@2x.png'),
+              )
+            ],
+          ),
+          // SizedBox(
+          //   height: .0,
+          // ),
+          Padding(
+            padding: EdgeInsets.only(left: 20.0),
+            child: Row(
+              children: [
+                Text(
+                  "User Interface designer competent at creating user friendly interfaces\nwith an easily navigable design. Translates high-level requirements \ninto necessary interaction flows and artifacts, specializing in creating\nclean artful; designs that are both intuitive and functional.",
+                  style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 9.8,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.black),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 13.5,
+          ),
+          Divider(
+            thickness: 1.0,
+          ),
+          SizedBox(
+            height: 13.5,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    '1420',
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 15,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black),
+                  ),
+                  Text(
+                    'Followers',
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 10,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xFFC9C9C9)),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  Text(
+                    '520',
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 15,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black),
+                  ),
+                  Text(
+                    'Following',
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 10,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xFFC9C9C9)),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Text(
+                    '600',
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 15,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black),
+                  ),
+                  Text(
+                    'Projects',
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 10,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xFFC9C9C9)),
+                  )
+                ],
+              )
+            ],
+          ),
+          SizedBox(
+            height: 13.5,
+          ),
+          Divider(
+            thickness: 1.0,
+          ),
+          SizedBox(
+            height: 15.5,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Recent Work',
+                      style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                WorkContainer(
+                  date: '15th',
+                  month: 'October',
+                  likes: '606',
+                  image: 'assets/images/fairy-tale-1077863_1280@2x.png',
+                ),
+                SizedBox(
+                  height: 18.0,
+                ),
+                WorkContainer(
+                  date: '21st',
+                  month: 'November',
+                  likes: '1453',
+                  image: 'assets/images/fairy-tale-1077863_12800@2x.png',
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class WorkContainer extends StatelessWidget {
+  const WorkContainer({
+    Key? key,
+    this.date,
+    this.image,
+    this.likes,
+    this.month,
+  }) : super(key: key);
+  final String? date;
+  final String? month;
+  final String? likes;
+  final String? image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 332.0,
+      height: 134.0,
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              Image.asset(image!),
+              Positioned(
+                left: 23.0,
+                bottom: 8.0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      date!,
+                      style: TextStyle(
+                          height: 1.0,
+                          fontFamily: 'Poppins',
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                    ),
+                    Text(
+                      month!,
+                      style: TextStyle(
+                          height: 1.0,
+                          fontFamily: 'Poppins',
+                          fontSize: 10,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xFFFFFFFF).withOpacity(0.7)),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                right: 26.0,
+                bottom: 11.0,
+                child: Container(
+                  width: 42.0,
+                  height: 42.0,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                          color: Colors.white.withOpacity(0.5), width: 1.0)),
+                  child: Center(
+                    child: Container(
+                      width: 36.0,
+                      height: 36.0,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                              color: Colors.white.withOpacity(0.5),
+                              width: 1.0)),
+                      child: Center(
+                          child: Image.asset(
+                        'assets/images/Icon awesome-play-circle@2x.png',
+                        width: 25.04,
+                        height: 25.04,
+                      )),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 6.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  SizedBox(
+                    width: 21.0,
+                  ),
+                  Container(
+                    width: 18.0,
+                    height: 18.0,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border:
+                            Border.all(color: Color(0xFFC9C9C9), width: 1.0)),
+                    child: Center(
+                      child: Icon(
+                        Icons.favorite,
+                        size: 12,
+                        color: Color(0xFFFF2424),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 3.0,
+                  ),
+                  Text(
+                    '$likes likes',
+                    style: TextStyle(
+                        height: 1.0,
+                        fontFamily: 'Poppins',
+                        fontSize: 10,
+                        fontWeight: FontWeight.w500,
+                        color: Color(0xFFFF2424)),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.add_box_rounded,
+                    color: Colors.black,
+                    size: 18,
+                  ),
+                  SizedBox(
+                    width: 29.4,
+                  ),
+                  Image.asset(
+                    'assets/images/Icon open-share-boxed@2x.png',
+                    width: 14.0,
+                  ),
+                  SizedBox(
+                    width: 21.0,
+                  ),
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
