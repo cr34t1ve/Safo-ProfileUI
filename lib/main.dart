e7ef1c4f8b0f7b28a4c55fb00e26ac06d082beea
cr34t1ve <cr34t1ve@protonmail.com>
2021-06-23 20:43:30 +0000

    routes added

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,14 @@ import 'package:safo_profileui/screens/home/home.dart';
 import 'package:safo_profileui/screens/profile/profile.dart';
 
 void main() {
-   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
   ));
   runApp(MaterialApp(
-    home: Home(),
+    initialRoute: '/',
+    routes: {
+      '/': (context) => Home(),
+      '/profile': (context) => Profile(),
+    },
   ));
 }
-
